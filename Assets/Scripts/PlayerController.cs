728f4e2d70e216216c6bf16294cb7d03e345f746
BJarv <brandon.jarv@gmail.com>
2015-11-09 12:23:22 -0800

    Init, Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public bool controllable = true;
+	Rigidbody2D r;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.Log ("horiz: " + Input.GetAxis("Horizontal") + "    vert: " + Input.GetAxis("Vertical"));
+		if(controllable){
+			/*if(Input.GetAxis("Horizontal") > 0) { //right
+
+			} else if (Input.GetAxis ("Horizontal") < 0) { //left
+
+			}
+
+			if(Input.GetAxis("Vertical") > 0) { //up
+				
+			} else if (Input.GetAxis ("Vertical") < 0) { //down
+				
+			}*/
+			r.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical"));
+
+		}
+	}
+}
